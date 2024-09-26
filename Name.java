179b71d98ff008e5869a2c6c98dae4bae1ba369e
Bhavya Reddy <karribhavyareddy6@gmail.com>
2024-09-26 17:50:25 +0530

    Intro

--- Name.java
+++ Name.java
@@ -1,5 +1,5 @@
-class Name{
-	public static void main(String[] args){
+class Name {
+	public static void main(String[] args) {
 		System.out.println("Hi everyone");
 		System.out.println("My name is Bhavya");
 	}
